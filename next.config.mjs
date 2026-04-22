b1cb6a5fc4257a056b1b0575d0c3312a2a1bd178
dennisw002 <dennis.wong002@umb.edu>
2026-04-22 01:25:49 -0400

    fixed cross-origin session cookie issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    distDir: './dist', // Changes the build output directory to `./dist/`.
+    distDir: './dist',
+    async rewrites() {
+        return [
+            { source: '/api/:path*',    destination: `${BACKEND}/api/:path*` },
+            { source: '/oauth2/:path*', destination: `${BACKEND}/oauth2/:path*` },
+            { source: '/login/:path*',  destination: `${BACKEND}/login/:path*` },
+            { source: '/logout',        destination: `${BACKEND}/logout` },
+        ]
+    },
 }
+
+const BACKEND = process.env.BACKEND_URL
+export default nextConfig
